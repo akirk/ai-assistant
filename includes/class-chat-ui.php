defe1d1bb94b3826bfed8b9153d2b25c136ae6c1
Alex Kirk <akirk@users.noreply.github.com>
2026-05-11 10:35:42 +0200

    Harden standalone assistant panel styles

--- includes/class-chat-ui.php
+++ includes/class-chat-ui.php
@@ -352,11 +352,10 @@ class Chat_UI {
                             <a href="' . $history_url . '" class="ai-header-link">' . $history . '</a>
                             <span class="ai-header-sep">|</span>
                             <a href="' . $settings_url . '" class="ai-header-link">' . $settings . '</a>
-                            <span class="ai-header-sep">|</span>
-                            <button type="button" id="ai-assistant-expand" class="ai-header-link" title="' . esc_attr__('Expand', 'ai-assistant') . '">
-                                <svg class="ai-expand-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M9.5 13.09l1.41 1.41-4.5 4.5H10v2H4v-6h2v3.59l4.5-4.5zm5-2.18L13.09 9.5l4.5-4.5H14v-2h6v6h-2V5.41l-4.5 4.5z"/></svg>
-                                <svg class="ai-collapse-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style="display:none"><path d="M14.5 10.91l-1.41-1.41 4.5-4.5H14v-2h6v6h-2V5.41l-4.5 4.5zm-5 2.18l1.41 1.41-4.5 4.5H10v2H4v-6h2v3.59l4.5-4.5z"/></svg>
-                            </button>
+	                            <span class="ai-header-sep">|</span>
+	                            <button type="button" id="ai-assistant-expand" class="ai-header-link" title="' . esc_attr__('Expand', 'ai-assistant') . '">
+	                                <svg class="ai-expand-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M9.5 13.09l1.41 1.41-4.5 4.5H10v2H4v-6h2v3.59l4.5-4.5zm5-2.18L13.09 9.5l4.5-4.5H14v-2h6v6h-2V5.41l-4.5 4.5z"/></svg>
+	                            </button>
                         </div>
                     </div>
                     <div id="ai-assistant-messages"></div>
